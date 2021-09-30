29505aabf67252604322dda43ef89a9ff34f9fee
urGitHubUsrName <mshabujbd06@gmail.com>
2021-09-30 22:14:11 +0600

    notfound added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/Header/Header';
+import NotFound from './components/NotFound/NotFound';
 import Shop from './components/Shop/Shop';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Header></Header>
       <Shop></Shop>
+      <NotFound></NotFound>
     </div>
   );
 }
